f4e304f82c270f3609099b383a2d6996dcf4fe43
Lori Henderson <lorim95@gmail.com>
2022-06-29 09:10:13 -0400

    commit tree d4a3f296768c00977e37e4c463fa7295ed40554d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number >= 1
+        if number >= 1
+            puts "#{number} SECOND(S)!"
+            puts number -= 1
+        elsif number == 0
+            "HAPPY NEW YEAR!"
+        end
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown)
+    puts countdown
 end
